30610be0260316a1fe79dbb680688565ef2f161b
tjq <729299276@qq.com>
2018-10-15 17:42:04 +0800

    v0.5

--- src/main/java/vip/wente/wtsystem/controller/RoomController.java
+++ src/main/java/vip/wente/wtsystem/controller/RoomController.java
@@ -2,12 +2,14 @@ package vip.wente.wtsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vip.wente.wtsystem.entity.Room;
 import vip.wente.wtsystem.service.IRoomService;
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @program: WtSystem
@@ -20,9 +22,14 @@ import java.util.Date;
 public class RoomController {
     @Autowired
     private IRoomService roomService;
+    //显示添加房间页面
+    @RequestMapping("/add")
+    public String showAdd(){
+        return "addRoom";
+    }
     //处理添加房间业务
     @RequestMapping("/addRoom")
-    public String addRoom(Integer roomNumber, Integer roomAmount, Double standardPriceDay, HttpSession session){
+    public String addRoom(String roomNumber, Integer roomAmount, Double standardPriceDay, HttpSession session){
         Integer shopNumber=(Integer) session.getAttribute("shopNumber");
         String createUser=(String) session.getAttribute("uname");
         Room room=new Room();
@@ -30,12 +37,17 @@ public class RoomController {
         room.setShopNumber(shopNumber);
         room.setRoomAmount(roomAmount);
         room.setRoomNumber(roomNumber);
+        room.setStandardPriceDay(standardPriceDay);
+        System.out.println("添加的房间信息："+room);
         roomService.addRoom(room);
         return "redirect:list";
     }
     //显示所有房间的信息
     @RequestMapping("/list")
-    public String showRoomList(){
-        return null;
+    public String showRoomList(HttpSession session, ModelMap map){
+        Integer shopNumber=(Integer) session.getAttribute("shopNumber");
+        List<Room> list=roomService.getAllRooms(shopNumber,null,null);
+        map.addAttribute("rooms",list);
+        return "roomList";
     }
 }
